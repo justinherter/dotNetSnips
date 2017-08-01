605ac148459a52228226eadd93741377ba756318
Justin Herter <jherter@asmdomain.local>
2017-08-01 14:30:39 -0700

    initial commit

---
+++ snips.cs
@@ -0,0 +1,11 @@
+namespace Snips
+{
+    public class DateTimeSnips {
+        public DateTime DaylightOffset(string timezone = null){
+            var timezone = timezone || "Pacific Standard Time";
+            DateTime utc = DateTime.UtcNow;
+            TimeZoneInfo zone = TimeZoneInfo.FindSystemTimeZoneById(timezone);
+            return TimeZoneInfo.ConvertTimeFromUtc(utc, zone);
+        }
+    }
+}
